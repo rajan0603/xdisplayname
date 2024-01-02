754d8a005e51a56e5f717d636269eaf264cf6e94
rajan <rajansuhagiya1234g@gmail.com>
2024-01-02 20:40:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -18,23 +18,23 @@ function App() {
   }
 
   return (
-    <form>
+    <div>
       <h1>Full Name Display</h1>
-      <div>
-        <label>First Name:</label>
-        <input type = "text" onChange = {(e) => setName(e.target.value)} required></input>
-      </div>
-      <div>
-        <label>Last Name:</label>
-        <input type = "text" onChange = {(e) => setLastName(e.target.value)} required></input>
-      </div>
-      <button type = "submit" onClick = {buttonClick}>Submit</button>
-      <div>
-        {isSubmit && <label>Full Name: {name} {lastName}</label>} 
-      </div>
-    </form>
-    
-    
+      <form onSubmit = {buttonClick}>
+        <div>
+          <label>First Name:</label>
+          <input type = "text" onChange = {(e) => setName(e.target.value)} required></input>
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input type = "text" onChange = {(e) => setLastName(e.target.value)} required></input>
+        </div>
+        <button type = "submit" >Submit</button>
+        <div>
+          {isSubmit && <label>Full Name: {name} {lastName}</label>} 
+        </div>
+      </form>
+    </div>
   );
 }
 
